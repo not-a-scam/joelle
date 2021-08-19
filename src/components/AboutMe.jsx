688c52397ed40033bcb0172453263fb3b3f9ff23
not-a-scam <zechariahyeo02@gmail.com>
2021-08-20 02:43:56 +0800

    update gh pages settings

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,9 +5,7 @@ function AboutMe() {
     return (
         <section id="aboutme" className="container">
             <div className="x-row">
-                <div class="border-img">
-                    <img src={aboutMeImg} alt="" />
-                </div>
+                <img src={aboutMeImg} alt="" />
                 <div className="aboutme-content">
                     <h2>about me</h2>
                     <p>I am a final year student at School of the Arts, the proud owner of 3 dogs and an avid fan of chocolate cake . The works here are part of my final showcase for my IB assessment and explore the theme of identity through various mediums.</p>
