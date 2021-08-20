a7546475451fe1179e1132b0c0cd28a80c393dde
not-a-scam <zechariahyeo02@gmail.com>
2021-08-20 13:57:08 +0800

    fix primary img

--- src/components/YRowCard.jsx
+++ src/components/YRowCard.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 function YRowCard(props) {
     return (
         <div className='y-row focus'>
-            {
-                props.imgs.map((img, index) => {
-                    return <img alt="" src={img} className='focus-img' key={index}/>
-                })
-            }
+            <div className='x-row'>
+                {
+                    props.imgs.map((img, index) => {
+                        return <img alt="" src={img} className='focus-img' key={index}/>
+                    })
+                }              
+            </div>
             <div className="row-content">
                 <h2>{props.title}</h2>
                 <p>{props.content}</p>
