0b7bfae1eb6998dbe98ea19b8ec5a846b5d431ac
not-a-scam <zechariahyeo02@gmail.com>
2021-08-20 14:00:08 +0800

    fix single y-row img

--- src/components/YRowCard.jsx
+++ src/components/YRowCard.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 function YRowCard(props) {
     return (
         <div className='y-row focus'>
-            <div className='x-row'>
-                {
-                    props.imgs.map((img, index) => {
+            {props.imgs.length === 1 
+                ? props.imgs.map((img, index) => {
                         return <img alt="" src={img} className='focus-img' key={index}/>
                     })
-                }              
-            </div>
+                :<div className='x-row'>
+                    {
+                        props.imgs.map((img, index) => {
+                            return <img alt="" src={img} className='focus-img' key={index}/>
+                        })
+                    }              
+                </div>}
             <div className="row-content">
                 <h2>{props.title}</h2>
                 <p>{props.content}</p>
